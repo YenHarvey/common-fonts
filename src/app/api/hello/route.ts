403a0fedee887542a3df779d6c4cef8338dacb1b
Yenharvey <cherrypeel@mewpaz.com>
2024-11-05 17:29:37 +0800

    init commit.

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,22 +1,17 @@
-import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
+import type { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  let responseText = 'Hello World'
+  const responseData = {
+    status: "success",
+    code: 0,
+    data: { message: "Hello Fonts!" },
+    timestamp: new Date().toISOString(),
+  };
 
-  // In the edge runtime you can use Bindings that are available in your application
-  // (for more details see:
-  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
-  //    - https://developers.cloudflare.com/pages/functions/bindings/
-  // )
-  //
-  // KV Example:
-  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
-
-  return new Response(responseText)
+  return new Response(JSON.stringify(responseData), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
